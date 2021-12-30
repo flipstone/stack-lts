a4dc2d282c1d05a059dfbe07f7c766190f7db51e
James Brechtel <git@jamesbrechtel.com>
2021-12-30 12:12:55 -0500

    Removes multi-stage build from the ARM image
    Turns out we need basically all the dev tools on the ARM image to
    support using LLVM so multi-stage build isn't buying us anything anyway.
    
    I also added steps to push the images after they're built.

--- v5/2.7.3/build.sh
+++ v5/2.7.3/build.sh
@@ -16,9 +16,11 @@ COMMAND=$1
 case $COMMAND in
   amd64)
     docker build --tag $AMD_TAG --file Dockerfile.amd64 .
+    docker push $AMD_TAG
     ;;
   arm64)
     docker build --tag $ARM_TAG --file Dockerfile.arm64 .
+    docker push $ARM_TAG
     ;;
   manifest)
     echo "Both $AMD_TAG and $ARM_TAG must be pushed to Docker Hub BEFORE running this step."
